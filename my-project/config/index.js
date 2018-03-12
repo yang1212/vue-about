5fad9ca98948be085866b12df52ef783ed762ac2
Tiboo <1204914685@qq.com>
2018-03-12 08:19:21 +0800

    组件

--- my-project/config/index.js
+++ my-project/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
@@ -37,8 +37,8 @@ module.exports = {
   },
 
   build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    // Template for error.html
+    index: path.resolve(__dirname, '../dist/error.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
